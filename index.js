04cf738963abbb9ec259066497e55604b2719ca6
xovest <79008917+xovest@users.noreply.github.com>
2021-11-28 21:48:46 +0200

    routing express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+//including routes
+app.use('/api', require('./routes'));
+
+//enabling cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server is on lclhst${PORT}`));
