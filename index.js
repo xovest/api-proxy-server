71170e9a45b75c70fe9bf35d4a422e7948d8eb7c
xovest <79008917+xovest@users.noreply.github.com>
2021-11-28 22:46:17 +0200

    limiting and caching

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+//rate limiting
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, //10 mins
+  max: 20
+});
+app.use(limiter);
+app.set('trust proxy', 1);
+
+//setting static folder
+app.use(express.static('public'));
 
 //including routes
 app.use('/api', require('./routes'));
