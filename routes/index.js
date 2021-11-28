fd0516d10fefcf492606d15ff3931a43a13593c0
xovest <79008917+xovest@users.noreply.github.com>
2021-11-28 22:05:49 +0200

    req and query

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const needle = require('needle');
+const url = require('url');
 
-router.get('/', (req, res) => {
-  res.json({ success: true });
+//env vars
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get('/', async (req, res) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`); 
+    const data = apiRes.body;
+
+    //logging the req to the public api
+    if (process.env.NODE_ENV !== 'production') {
+      console.log(`Req: ${API_BASE_URL}?${params}`);
+    }
+  
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+
 });
 
 module.exports = router;
